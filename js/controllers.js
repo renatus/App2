a7324a5f4c508cc2427b09cc98941a5ada18c2f1
renatus <pub@renat.biz>
2014-03-30 04:24:17 +0400

    Use IndexedDB to store data

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,6 @@
 var phonecatApp = angular.module('startApp', ['exoFilters']);
  
 phonecatApp.controller('StartCtrl', function ($scope) {
-	$scope.phones = [
-		{'name': 'Nexus S',
-		 'snippet': 'Fast just got faster with Nexus S.'},
-		{'name': 'Motorola XOOM™ with Wi-Fi',
-		 'snippet': 'The Next, Next Generation tablet.'},
-		{'name': 'MOTOROLA XOOM™',
-		 'snippet': 'The Next, Next Generation tablet.'}
-	];
 	
 	$scope.activities = [
 		{"nid":"6650","language":"English","title":"End an agreements with Stream ISP","status":"Completed","statusRAW":"completed","priority":"Low","priorityRAW":"1000","strategicImportance":"Low","strategicImportanceRAW":"1000","difficultyPlanned":"Hard","difficultyPlannedRAW":"3000","difficulty":"Hard","difficultyRAW":"3000","group":"Flat 524, Biryulyovo, flat 464, MTS, Internet","groupRAW":"742, 760, 1501, 472","dateTimePlannedStart":"2014-03-01 11:00:50","dateTimePlannedEnd":"2014-03-19 18:00:24","dateTimePlannedTZ":"Europe/Moscow Europe/Moscow","dateTimePlannedOffset":"14400 14400","dateTimeStart":"2014-03-07 04:01:53","dateTimeEnd":"2014-03-07 04:01:56","dateTimeTZ":"Europe/Moscow Europe/Moscow","dateTimeOffset":"14400 14400","lastUpdated":"1394150475","bodySummary":""},
@@ -28,6 +20,20 @@ phonecatApp.controller('StartCtrl', function ($scope) {
         
         return false; // otherwise it won't be within the results
     };
+	
+	
+	
+	function init(){
+		indexedDBexo.open().then(function(){
+			alert('DB opened');
+		});
+	}
+	
+	init();
+	
+	
+	
+	
 
 	
    
@@ -35,38 +41,105 @@ phonecatApp.controller('StartCtrl', function ($scope) {
 
 
 
-    angular.module('exoFilters', []).filter('reverse', function() {
-        return function(input, uppercase) {
-            input = input || '';
-            var out = "";
-            //if(input){
-                for (var i = 0; i < input.length; i++) {
-                    out = input.charAt(i) + out;
-                }
-            //}
-            
-            // conditional based on optional argument
-            if (uppercase) {
-                out = out.toUpperCase();
-            }
-            
-            return out;
-        };
-    });
 
-/*
-filter('custfilter', function () {
-    return function (movies, genres) {
-        var items = {
-            genres: genres,
-            out: []
-        };
-        angular.forEach(movies, function (value, key) {
-            if (this.genres[value.genre] === true) {
-                this.out.push(value);
-            }
-        }, items);
-        return items.out;
-    };
+
+phonecatApp.factory('indexedDBexo', function($window, $q){
+	
+	//IndexedDB database name
+	var dbName = "ExocortexDB";
+	//Database version (should be increased, when structure updates). Should be of integer type.
+	var dbVersion = 4;
+	var exoDB = {};
+	var indexedDB = window.indexedDB;
+	
+	exoDB.indexedDB = {};
+	exoDB.indexedDB.db = null;
+	
+	//Handle DB-related errors
+	exoDB.indexedDB.onerror = function(e) {
+		console.log(e);
+	};
+	
+	
+	
+	//Function to open DB and upgrade it's data structure, if needed
+	exoDB.indexedDB.open = function() {
+		var deferred = $q.defer();
+		
+		//Request to open database. Will return IDBOpenDBRequest object.
+		var request = indexedDB.open(dbName, dbVersion);
+		
+		request.onsuccess = function(e) {
+			console.log ("DB " + dbName + " was opened and ready for work");
+			exoDB.indexedDB.db = e.target.result;
+			deferred.resolve();
+		}
+		
+		request.onupgradeneeded = function(e) {
+			exoDB.indexedDB.db = e.target.result;
+			var db = exoDB.indexedDB.db;
+			console.log ("Going to upgrade DB from version "+ e.oldVersion + " to version " + e.newVersion);
+			
+			//If there is Object store with the same name at DB from previous revision,
+			//we'll face error while trying to upgrade DB
+			//We should delete existing Object store (and all it's data, of course)
+			try {
+				if (db.objectStoreNames && db.objectStoreNames.contains("activities")) {
+					db.deleteObjectStore("activities");
+				}
+			}
+			catch (err) {
+				console.log("Error in objectStoreNames: " + err);
+			}
+			
+			//Create object store
+			//Object Store is a storage for objects, instead of tables at SQL databases
+			//We do not define objects structure here other than "fields" for keyPath, and for indexes
+			//While adding objects, you can omit fields, including indexing ones, but keyPath field should be filled
+			//We can make one of it's "fields" (with unique values) an in-line key with keyPath
+			var store = db.createObjectStore("activities", {keyPath: "timeStamp"});
+			// Create an index to search customers by text field. We may have duplicates so we can't use a unique index.
+			store.createIndex("activities", "activities", {unique: false});
+			
+			//Or we can make unique integer out-of-line keys (1,2,3 ...) with keyGenerator, enabled by {autoIncrement: true}
+			//var store = db.createObjectStore("store2", {autoIncrement: true});
+			//console.log("Onupgradeneeded: "+ JSON.stringify(store));
+		}
+		
+		request.onfailure = function(e) {
+			console.error("Failed to open DB: " + e);
+			deferred.reject();
+		}
+		
+		request.onerror = function(e) {
+			console.error("Error while opening DB: " + e);
+			deferred.reject();
+		}
+		
+		return deferred.promise;
+	};
+	
+}
+
+
+
+
+
+
+
+angular.module('exoFilters', []).filter('reverse', function() {
+	return function(input, uppercase) {
+		input = input || '';
+		var out = "";
+		for (var i = 0; i < input.length; i++) {
+			out = input.charAt(i) + out;
+		}
+		
+		// conditional based on optional argument
+		if (uppercase) {
+			out = out.toUpperCase();
+		}
+		
+		return out;
+	};
 });
-*/
