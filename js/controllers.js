8eb4441fa76a2d6d1cdd2795519d2953fa9f47c9
renatus <pub@renat.biz>
2014-03-31 02:35:54 +0400

    Use IndexedDB to store data

--- js/controllers.js
+++ js/controllers.js
@@ -38,9 +38,13 @@ phonecatApp.controller('StartCtrl', function ($scope, MathService, indexedDBexo)
 
 
 phonecatApp.service('MathService', function() {
+	var deferred = $q.defer();
     this.add = function(a, b) { return a + b };
     this.subtract = function(a, b) { return a - b };
-    this.multiply = function(a, b) { return a * b };
+    this.multiply = function(a, b) { 
+		deferred.resolve();
+		return a * b;
+	};
     this.divide = function(a, b) { return a / b };
 });
 
