98a8ade18682ff931af18853d7e51d615f30fae7
renatus <pub@renat.biz>
2014-04-03 20:41:55 +0400

    Use IndexedDB

--- js/controllers.js
+++ js/controllers.js
@@ -40,7 +40,7 @@ phonecatApp.controller('StartCtrl', function ($scope, indexedDBexo) {
 	$scope.init();
     
     $scope.addEnttry = function(){
-        indexedDBexo.addEntry('T3').then(function(){
+        indexedDBexo.addEntry(activity.title).then(function(){
             console.log('Activity added!');
         });
     }
