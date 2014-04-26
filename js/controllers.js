a85c96314d4e4d84671eb645343bcfbbca8c74bd
renatus <pub@renat.biz>
2014-04-26 16:41:23 +0400

    Use JSCS

--- js/controllers.js
+++ js/controllers.js
@@ -1,36 +1,36 @@
 var phonecatApp = angular.module('startApp', ['exoFilters']);
- 
+
 phonecatApp.controller('StartCtrl', function ($scope, indexedDBexo) {
-	
-	
-	
+
+
+
 	$scope.manyEntries = [];
 	var allEntries = [];
-	
-	for(var i=0; i<1000; i++) {
+
+	for (var i = 0; i < 1000; i++) {
 		var newEntry = {"nid":i, "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-		
+
 		allEntries.push(newEntry);
 		//$scope.manyEntries.push(newEntry);
 	}
 	$scope.manyEntries = allEntries;
-	
-	
-	
+
+
+
 	$scope.filter50 = function(manyEntry){
-        
+
         if(manyEntry.nid < $scope.manyNID && manyEntry.nid > ($scope.manyNID - 50)){
             return true; // this will be listed in the results
         }
-        
+
         return false; // otherwise it won't be within the results
     };
-		
-		
-		
-		
-	
-    
+
+
+
+
+
+
 	$scope.activities = [
 		{"nid":"6650","language":"English","title":"End an agreements with Stream ISP","status":"Completed","statusRAW":"completed","priority":"Low","priorityRAW":"1000","strategicImportance":"Low","strategicImportanceRAW":"1000","difficultyPlanned":"Hard","difficultyPlannedRAW":"3000","difficulty":"Hard","difficultyRAW":"3000","group":"Flat 524, Biryulyovo, flat 464, MTS, Internet","groupRAW":"742, 760, 1501, 472","dateTimePlannedStart":"2014-03-01 11:00:50","dateTimePlannedEnd":"2014-03-19 18:00:24","dateTimePlannedTZ":"Europe/Moscow Europe/Moscow","dateTimePlannedOffset":"14400 14400","dateTimeStart":"2014-03-07 04:01:53","dateTimeEnd":"2014-03-07 04:01:56","dateTimeTZ":"Europe/Moscow Europe/Moscow","dateTimeOffset":"14400 14400","lastUpdated":"1394150475","bodySummary":""},
 		{"nid":"6090","language":"Russian","title":"\u041e\u043f\u043b\u0430\u0442\u0430 \u0438\u043d\u0442\u0435\u0440\u043d\u0435\u0442\u0430, \u041c\u0422\u0421, +7(916)5477868","status":"Completed","statusRAW":"completed","priority":"Major","priorityRAW":"3000","strategicImportance":"Low","strategicImportanceRAW":"1000","difficultyPlanned":"Normal","difficultyPlannedRAW":"2000","difficulty":"Normal","difficultyRAW":"2000","group":"Internet, Payments, Phone, MTS","groupRAW":"472, 743, 1191, 1501","dateTimePlannedStart":"2014-03-31 00:00:43","dateTimePlannedEnd":"2014-04-03 23:54:19","dateTimePlannedTZ":"Europe/Moscow Europe/Moscow","dateTimePlannedOffset":"14400 14400","dateTimeStart":"2014-03-07 04:02:30","dateTimeEnd":"2014-03-07 04:02:33","dateTimeTZ":"Europe/Moscow Europe/Moscow","dateTimeOffset":"14400 14400","lastUpdated":"1394150513","bodySummary":""},
@@ -124,7 +124,7 @@ phonecatApp.service('indexedDBexo', function($window, $q){
 		request.onupgradeneeded = function(e) {
 			exoDB.indexedDB.db = e.target.result;
 			var db = exoDB.indexedDB.db;
-			console.log ("Going to upgrade DB from version "+ e.oldVersion + " to version " + e.newVersion);
+			console.log ("Going to upgrade DB from version " + e.oldVersion + " to version " + e.newVersion);
 			
 			//If there is Object store with the same name at DB from previous revision,
 			//we'll face error while trying to upgrade DB
@@ -222,9 +222,9 @@ phonecatApp.service('indexedDBexo', function($window, $q){
         
         cursorRequest.onsuccess = function(e) {
             var result = e.target.result;
-            if(result === null || result === undefined){
+            if (result === null || result === undefined) {
                 deferred.resolve(activities);
-            }else{
+            } else {
                 if(result){
                     activities.push(result.value);
                     result.continue();
