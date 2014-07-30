65639cae5605b194652b8d38a32b29541104c47b
renatus <pub@renat.biz>
2014-07-30 05:22:57 +0400

    Make it possible to login to server

--- js/main.js
+++ js/main.js
@@ -292,7 +292,7 @@ app.service('UUID4', function(){
 //Controller to start communication with server, when user initiated it
 app.controller('serverInteract', function ($scope, $q, backend, exoSettings, setSettings) {
     $scope.pageLogin = {};
-    $scope.pageLogin.backendURL = backend.getCurDomain;
+    $scope.pageLogin.backendURL = "backend.getCurDomain";
     
     //Method to initiate logging process, when user pressed Login button
     $scope.login = function(pageLogin){
