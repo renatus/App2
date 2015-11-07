410eff4481b602df481a493903c47df72571b47f
renatus <pub@renat.biz>
2015-11-07 15:45:27 +0300

    Test $mdDialog.alert

--- js/main.js
+++ js/main.js
@@ -688,25 +688,32 @@ app.service('backendSyncAll', function($rootScope, backendSync){
 //Service to work with random interface elements
 app.service('userInterface', function($window){
 
-    // Appending dialog to document.body to cover sidenav in docs app
-    // Modal dialogs should fully cover application
-    // to prevent interaction outside of dialog
-    $mdDialog.show(
-        $mdDialog.alert()
-        .parent(angular.element(document.querySelector('#popupContainer')))
-        .clickOutsideToClose(true)
-        .title('This is an alert title')
-        .content('You can specify some description text in here.')
-        .ariaLabel('Alert Dialog Demo')
-        .ok('Got it!')
-        .targetEvent(ev)
-    );
-
-
-
 	//Method to notify user about something by Alert
     //alertBody argument should contain message text
 	this.alert = function(alertBody) {
+
+
+
+
+        // Appending dialog to document.body to cover sidenav in docs app
+        // Modal dialogs should fully cover application
+        // to prevent interaction outside of dialog
+        $mdDialog.show(
+            $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('This is an alert title')
+            .content('You can specify some description text in here.')
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Got it!')
+            .targetEvent(ev)
+        );
+
+
+
+
+
+
         //Show alert message to the user
         $window.alert(alertBody);
     }
